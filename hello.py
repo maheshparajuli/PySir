789df475d71a4804c57f62a326128df6c1267d30
maheshparajuli <maheshparajuli717@gmail.com>
2025-10-05 17:14:34 +0545

    ooooo

--- hello.py
+++ hello.py
@@ -1,7 +1,13 @@
 from flask import Flask
+from flask import request
+from markupsafe import escape
 
 app=Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, world</p>"
+
+
+@app.route("/hello")
+def hello():
+    name = request.args.get("name", "Flask") #URL bata name parameter ko value ley bhaneko ho
+    return f"Hello, {escape(name)}!"
+
