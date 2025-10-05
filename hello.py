0fc064c434549ead408df4827aa6244eb38e3096
maheshparajuli <maheshparajuli717@gmail.com>
2025-10-05 21:51:02 +0545

    ok

--- hello.py
+++ hello.py
@@ -6,22 +6,33 @@ app=Flask(__name__)
 
 
 
-@app.route("/hello")
+@app.route("/")
+def index():
+    return 'Index page'
+
+@app.route('/hello')
 def hello():
+    return "K xa Sarkar?"
+
+
+# def hello():
     """request.args.get("name", "Flask") means:
 
 Get the value of name from the URL query.
 If it doesnt exist, use 'Flask' as the default."""
-    name = request.args.get("name", "Flask") #URL bata name parameter ko value ley bhaneko ho
-    return f"Hello, {escape(name)}!"
+    # name = request.args.get("name", "Flask") #URL bata name parameter ko value ley bhaneko ho
+    # return f"Hello, {escape(name)}!"
 
 """
-request in flask: the request object is used to access data sent 
+request in flask: the request object is used to access data sent
 by the client (browser, app, etc.) to the server.
 1. request.args: Access query parameters (from URL, e.g., ?name=Mahesh)
 
 markupsafe: Flask imports escape from markupsafe internally.
 So you can use escape() in Flask without installing markupsafe separately
  (because Flask already installs it as a dependency).
- 
+
+ Escaping prints malicious text as text without rendering it to the users browser.
+
+
 """
