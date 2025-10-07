c8c493b10617846ed35075ba73c754463605a409
maheshparajuli <maheshparajuli717@gmail.com>
2025-10-07 11:45:20 +0545

    going on brother

--- entry.py
+++ entry.py
@@ -2,21 +2,21 @@ from flask import Flask
 from flask import request
 from markupsafe import escape
 
+def check():
+    return 'Thik xa, info correct xa timro, tara website nei banya xaina'
+def formi():
+    return 'form nei banako xaina, kehi din ajhai lagxa'
+
 app=Flask(__name__)
 
+@app.route('/ja_jatha',methods=['GET','POST'])
+def login():
+    if request.method=='POST':
+        return check()
+    
+    else:
+        return formi()
 
-@app.route('/user/<username>')
 
-def show_user_profile(username):
-    # show the user profile for that user
-    return f'User {escape(username)}'
 
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
-    # show the post with the given id, the id is an integer
-    return f'Post {post_id}'
 
-@app.route('/path/<path:subpath>')
-def show_subpath(subpath):
-    # show the subpath after /path/
-    return f'Subpath {escape(subpath)}'
