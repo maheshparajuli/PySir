f3049154d5c8cb69454e7f52ef6dc3b98c2d5c11
maheshparajuli <maheshparajuli717@gmail.com>
2025-10-08 21:20:34 +0545

    ok

--- entry.py
+++ entry.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import request
 from markupsafe import escape
+from flask import render_template
 
 """
 def check():
@@ -25,7 +26,11 @@ def login():
 
 app=Flask(__name__)
 
-@app.route()
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', person=name)
 
 
 
