7bf9e8bd96a2f7a87e158c33b4c97267148e0c1a
maheshparajuli <maheshparajuli717@gmail.com>
2025-10-09 15:58:45 +0545

    ss

--- entry.py
+++ entry.py
@@ -44,3 +44,8 @@ Automatic escaping → protects you by showing raw HTML as text.
 
 
 """
+with app.test_request_context('/hello', method='POST'):
+    # now you can do something with the request until the
+    # end of the with block, such as basic assertions:
+    assert request.path == '/hello'
+    assert request.method == 'POST'
